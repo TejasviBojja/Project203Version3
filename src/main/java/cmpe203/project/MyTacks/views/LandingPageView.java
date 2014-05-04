4176ffa443c6e26e5162530f30cc7db1770c4e17
TejasviBojja <tejasvi.bojja@gmail.com>
2014-05-03 17:19:58 -0700

    commit3

--- src/main/java/cmpe203/project/MyTacks/views/LandingPageView.java
+++ src/main/java/cmpe203/project/MyTacks/views/LandingPageView.java
@@ -4,8 +4,15 @@ import com.yammer.dropwizard.views.View;
 
 public class LandingPageView extends View{
 	
-	public LandingPageView(){
+	private final String name;
+	
+	public LandingPageView(String firstName)extends View{
 		super("/LandingPage.ftl");
+		this.name=firstName;
 	}
 
+	public String getName(){
+		return name;
+	}
+	
 }
