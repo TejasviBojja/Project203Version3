0bdc4a7bbad90346b432b8b9351abfe71ed000c6
TejasviBojja <tejasvi.bojja@gmail.com>
2014-05-05 12:37:24 -0700

    Comment6

--- src/main/java/cmpe203/project/MyTacks/api/resources/EditBoardSucessResource.java
+++ src/main/java/cmpe203/project/MyTacks/api/resources/EditBoardSucessResource.java
@@ -8,15 +8,12 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.Response;
 
-
-
-
-
 //import cmpe203.project.MyTacks.domain.Email;
 import cmpe203.project.MyTacks.views.CreateBoardView;
 import cmpe203.project.MyTacks.views.EditBoardSuccessView;
 import cmpe203.project.MyTacks.views.EditBoardView;
 
+//Page shown after successful edit to boards
 @Path("/EditBoardSuccess")
 public class EditBoardSucessResource {
 	public EditBoardSucessResource()
@@ -24,6 +21,7 @@ public class EditBoardSucessResource {
 	
 	}
 
+//Call to get the view after the board is edited
 	@GET
 	public EditBoardSuccessView getEditProfileSuccess()
 	{
