3bcd18a2fa647e1bfd617b85f88959c47deed410
TejasviBojja <tejasvi.bojja@gmail.com>
2014-05-05 12:33:45 -0700

    Comment6

--- src/main/java/cmpe203/project/MyTacks/api/resources/EditBoardResource.java
+++ src/main/java/cmpe203/project/MyTacks/api/resources/EditBoardResource.java
@@ -15,6 +15,7 @@ import javax.ws.rs.core.Response;
 import cmpe203.project.MyTacks.views.CreateBoardView;
 import cmpe203.project.MyTacks.views.EditBoardView;
 
+//Page for editing the board created
 @Path("/EditBoard")
 public class EditBoardResource {
 	public EditBoardResource()
@@ -22,6 +23,7 @@ public class EditBoardResource {
 	
 	}
 
+//To call the view for editing the already created board
 	@GET
 	public EditBoardView getEditProfileSuccess()
 	{
