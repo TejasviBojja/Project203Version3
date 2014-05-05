add468997feaba2741a71a352470cd0122c8da10
richasingh510 <richa.singh510@gmail.com>
2014-05-05 15:22:56 -0700

    comment edit1

--- src/main/java/cmpe203/project/MyTacks/views/EditBoardSuccessView.java
+++ src/main/java/cmpe203/project/MyTacks/views/EditBoardSuccessView.java
@@ -1,6 +1,9 @@
 package cmpe203.project.MyTacks.views;
 import com.yammer.dropwizard.views.View;
 
+/**
+ * This class displays the successful message after editing the board through this view page 
+ */ 
 public class EditBoardSuccessView extends View{
 	public EditBoardSuccessView(){
 		super("/EditBoardSucess.ftl");
