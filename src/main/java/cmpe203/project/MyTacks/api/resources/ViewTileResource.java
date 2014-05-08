d86c68de176055b2ffbf8ecaa97738bcc62cc274
diptichavan <fordipti1@gmail.com>
2014-05-08 00:52:24 -0700

    commit

--- src/main/java/cmpe203/project/MyTacks/api/resources/ViewTileResource.java
+++ src/main/java/cmpe203/project/MyTacks/api/resources/ViewTileResource.java
@@ -11,17 +11,14 @@ import cmpe203.project.MyTacks.views.ViewBoardView;
 @Path("/ViewTile")
 public class ViewTileResource {
 
-	
 
-
-public class ViewBoardResource {
 	
-	@GET
-	public  ViewBoardView getSignUp(@QueryParam("email") String email)
+	/*@GET
+	public  ViewTileView getTile(@QueryParam("email") String email,@QueryParam("boardName") String boardName)
 	{
 		mongo test=new mongo();
 		List board=test.getUserDetails(email);
 		System.out.print("boards in resource class is ::"+board);
 		return new ViewBoardView(board,email);
-	}
+	}*/
 }
